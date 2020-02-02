71c6b017e2160f96958fd500367cb477ba5d7817
Shun Kakinoki <shunkakinoki@gmail.com>
2020-02-02 19:06:32 +0900

    Changed file /Users/shunkakinoki/ghq/github.com/asyncmd/asyncmd/src/1.md, Auto-Commit V0.1

--- auto-commit.sh
+++ auto-commit.sh
@@ -3,6 +3,9 @@
 NEW_UUID=$(LC_ALL=C tr -dc 'A-Za-z0-9' </dev/urandom | head -c 13)
 BRANCH_NAME=auto-commit/$NEW_UUID
 
+git checkout master
+git pull
+
 if [ `git branch --list $BRANCH_NAME` ]
 then
     git checkout -b $BRANCH_NAME
@@ -15,6 +18,5 @@ else
     git commit -am "Changed file $*, Auto-Commit V0.1"
     git push --set-upstream origin $(git branch-name)
     hub pull-request --no-edit
-    git checkout master
-    git pull
+    
 fi
