db968bd43ffeee27f8c02241636558f18a44b150
Shun Kakinoki <shunkakinoki@gmail.com>
2020-02-02 19:10:42 +0900

    Changed file /Users/shunkakinoki/ghq/github.com/asyncmd/asyncmd/src/1.md, Auto-Commit V0.1

--- auto-commit.sh
+++ auto-commit.sh
@@ -17,6 +17,5 @@ else
     git add src
     git commit -am "Changed file $*, Auto-Commit V0.1"
     git push --set-upstream origin $(git branch-name)
-    hub pull-request --no-edit
-    
+    hub pull-request --no-edit --draft
 fi
