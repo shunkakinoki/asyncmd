8ed27e00fd04eb9a6b92214eee88d312ddd6abbd
Shun Kakinoki <shunkakinoki@gmail.com>
2020-02-02 16:36:49 +0900

    Changed file /Users/shunkakinoki/ghq/github.com/asyncmd/asyncmd/src/1.md, Auto-Commit V0.1

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 git checkout -b auto-commit
-git add --all
+git add src
 git commit -am "Changed file $*, Auto-Commit V0.1"
 git push --set-upstream origin $(git branch-name)
+git checkout master
+git pull
