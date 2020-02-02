d866166853a6cf2d5547524756e2b15292f23ce3
Shun Kakinoki <shunkakinoki@gmail.com>
2020-02-02 16:39:54 +0900

    Changed file /Users/shunkakinoki/ghq/github.com/asyncmd/asyncmd/src/1.md, Auto-Commit V0.1

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 git checkout -b auto-commit/$NEW_UUID
 git add --all
 git commit -am "Changed file $*, Auto-Commit V0.1"
