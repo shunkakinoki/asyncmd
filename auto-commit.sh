e1376804e223f69d05be3136566b68d5d7a85b6c
Shun Kakinoki <shunkakinoki@gmail.com>
2020-02-02 16:59:19 +0900

    Changed file /Users/shunkakinoki/ghq/github.com/asyncmd/asyncmd/src/1.md, Auto-Commit V0.1

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-git checkout -b auto-commit
-git add src
-git commit -am "Changed file $*, Auto-Commit V0.1"
-git push --set-upstream origin $(git branch-name)
-<<<<<<< HEAD
-hub pull-request --no-edit
-=======
->>>>>>> ee7d1b7e2019a9600191824a53ae4829faea38ee
-git checkout master
-git pull
+NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+BRANCH_NAME=auto-commit/$NEW_UUID
+
+if [ `git branch --list $BRANCH_NAME` ]
+then
+    git add src
+    git commit -am "Changed file $*, Auto-Commit V0.1"
+    git push
+else
+    git checkout -b $BRANCH_NAME
+    git add src
+    git commit -am "Changed file $*, Auto-Commit V0.1"
+    git push --set-upstream origin $(git branch-name)
+    hub pull-request --no-edit
+    git checkout master
+    git pull
+fi
