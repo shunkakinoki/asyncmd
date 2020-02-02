dcfbac0961621d4d591ca51a9d2a26f9bfd6e92c
Shun Kakinoki <shunkakinoki@gmail.com>
2020-02-02 16:20:34 +0900

    init async md

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM williamyeh/fswatch:1.1
+
+COPY auto-commit.sh auto-commit.sh
---
+++ auto-commit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git checkout -b auto-commit
+git add --all
+git commit -am "Changed file $*, Auto-Commit V0.1"
+git push --set-upstream origin $(git branch-name)
